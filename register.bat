cdef7a78aefd2f60eca97d060cdf7b78fcf7d9ef
Michael Richardson <michaelgocougs@gmail.com>
2018-07-13 10:43:47 -0700

    first commit

---
+++ register.bat
@@ -0,0 +1,1 @@
+curl -v -XPOST http://192.168.1.9/sony/accessControl -d '{"method":"actRegister","params":[{"clientid":"TVSideView:4e6b4a7a-aa52-416e-bfad-6aac6f560f9d","nickname":"EventGhost (TV SideView)","level":"private"},[{"value":"yes","function":"WOL"}]],"id":8,"version":"1.0"}'
